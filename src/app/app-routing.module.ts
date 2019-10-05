861614c642f8bdb6d68b5d1434fd49e336ea0a56
bhandhariya <sainiraja59@gmail.com>
2019-10-05 12:09:44 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CollectionComponent } from './collection/collection.component';
+import { AboutComponent } from './about/about.component';
+import { ServiceComponent } from './service/service.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:HomeComponent},
+  {path:"home",component:HomeComponent},
+  {path:"collection",component:CollectionComponent},
+  {path:"about",component:AboutComponent},
+  {path:"service",component:ServiceComponent},
+  {path:"blog",component:BlogComponent},
+  {path:"contact",component:ContactComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AsideComponent } from './aside/aside.component';
+import { FooterComponent } from './footer/footer.component';
+import { InstagramComponent } from './instagram/instagram.component';
+import { DescComponent } from './desc/desc.component';
+import { CollectionComponent } from './collection/collection.component';
+import { AboutComponent } from './about/about.component';
+import { ServiceComponent } from './service/service.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AsideComponent,
+    FooterComponent,
+    InstagramComponent,
+    DescComponent,
+    CollectionComponent,
+    AboutComponent,
+    ServiceComponent,
+    BlogComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
